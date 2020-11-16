919b0137b097c11a4c6f8ebcc36737e7cf98a656
Will103.SIT <varitchai.will@mail.kmutt.ac.th>
2020-11-16 14:34:23 +0700

    avenger

--- FoodSelection/src/FoodRandom/Foodarray.java
+++ FoodSelection/src/FoodRandom/Foodarray.java
@@ -23,7 +23,7 @@ public class Foodarray {
         System.out.println("ทดสอบ");
         System.out.println("TEST");
         System.out.println("I love pornhub");
-        System.out.println("I love hentai");
+        System.out.println("I love avenger");
 
     }
 }
