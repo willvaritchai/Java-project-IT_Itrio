36c042ee56be60a2793f1b82d5c987f331130610
Will103.SIT <varitchai.will@mail.kmutt.ac.th>
2020-11-16 14:02:13 +0700

    food array

--- FoodSelection/src/FoodRandom/Foodarray.java
+++ FoodSelection/src/FoodRandom/Foodarray.java
@@ -17,7 +17,9 @@ public class Foodarray {
 
         String[] randomfoodItalian1 = {"Tuna Salad 273 THB", "Big Mama Salad 279 THB", "Pizza Salmon 412 THB", "Crab Spaghetti 343 THB" , "Mussels Baked With Cheese 380 THB"};
         Random r = new Random();
-        int randomfunction = r.nextInt(randomfoodItalian1.length);
+        int randomfunction = r.nextInt(randomfoodItalian1.length); 
         System.out.println(randomfoodItalian1[randomfunction]);
+        System.out.println("");
+        System.out.println("ทดสอบ");
     }
 }
