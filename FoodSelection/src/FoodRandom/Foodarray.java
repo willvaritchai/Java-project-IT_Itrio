4e550644db58ccc708f6138ce43c3987c3f5ef5f
Student <Student@TRAIN1-2PC48>
2020-11-17 15:56:24 +0700

    return to past

--- FoodSelection/src/FoodRandom/Foodarray.java
+++ FoodSelection/src/FoodRandom/Foodarray.java
@@ -15,12 +15,10 @@ public class Foodarray {
 
     public static void main(String[] args) {
 
-        String[] randomfoodItalian1 = {"Tuna Salad 273 THB", "Big Mama Salad 279 THB", "Pizza Salmon 412 THB", "Crab Spaghetti 343 THB" , "Mussels Baked With Cheese 380 THB"};
+        String[] randomfoodItalian1 = {"Tuna Salad 273 THB", "Big Mama Salad 279 THB", "Pizza Salmon 412 THB", "Crab Spaghetti 343 THB", "Mussels Baked With Cheese 380 THB"};
         Random r = new Random();
-        int randomfunction = r.nextInt(randomfoodItalian1.length); 
-        System.out.println(randomfoodItalian1[randomfunction]); 
-        
-        System.out.println("Hello WillGay555");
-        
+        int randomfunction = r.nextInt(randomfoodItalian1.length);
+        System.out.println(randomfoodItalian1[randomfunction]);
+
     }
 }
